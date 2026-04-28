2d0beadc1c52d91d041adaa3084792a402fe9dac
krishpy <43674077+krishpy@users.noreply.github.com>
2026-04-29 03:05:39 +0530

    implemented ensureAnonymousUserId();

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,31 @@ import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
+
+function ensureAnonymousUserId() {
+  const KEY = "tonecheck_user_id";
+
+  let id = localStorage.getItem(KEY);
+
+  if (!id) {
+    id =
+      "tc_" +
+      Date.now().toString(36) +
+      Math.random().toString(36).substring(2, 10);
+
+    localStorage.setItem(KEY, id);
+
+    // optional track first-time visitor in Plausible
+    if (window.plausible) {
+      window.plausible("New User");
+    }
+  }
+
+  return id;
+}
+
+ensureAnonymousUserId();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
